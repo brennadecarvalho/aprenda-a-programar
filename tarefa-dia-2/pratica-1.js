c99ee54d25d0e3c56e4970a374e26f5f63676a8d
Brenna de Carvalho <brennadcarvalho@gmail.com>
2022-08-24 19:11:48 -0300

    Pratica-JS-parte-3

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,8 +1,11 @@
-let dados = {
-nome:"Brenna",
-email:"brennadcarvalho@gmail.com",
-profissao:"estudante",
-lugarParaConhecer:"Europa" 
-};
 
-console.log(dados);
+let nome = "Brenna";
+let email = "brennadcarvalho@gmail.com";
+let profissao = "estudante";
+let idade = 25;
+let lugarParaConhecer = "Europa" ;
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarParaConhecer}.
+Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? 
+Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
