0c76a8ba4c3f75590b2307208208e5fb93cc0f25
Brenna de Carvalho <brennadcarvalho@gmail.com>
2022-08-27 16:36:09 -0300

    Estruturas de Repetição

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,8 @@
+let multiplicador = 8
+let n = 10
+let i = 1
+
+while(i <= n) {
+    console.log(`${multiplicador} x ${i} = ${multiplicador * 1}`);
+    i++
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,7 @@
+let i = 1
+let n = 16
+
+while (i < n) {
+    console.log(`Este é um número ímpar: ${i}`)
+     i += 2
+}
